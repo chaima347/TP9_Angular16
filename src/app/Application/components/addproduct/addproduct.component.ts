e54d98bfe6e14bedf8b02ba5f116534ed7a9f7ec
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-13 14:59:28 +0100

    Partie 3 Application TP9 finie

--- src/app/Application/components/addproduct/addproduct.component.ts
+++ src/app/Application/components/addproduct/addproduct.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Category } from '../../enum/category';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../classes/product';
@@ -12,15 +12,19 @@ import { Product } from '../../classes/product';
 export class AddproductComponent implements OnInit {
   produits: Product[] = [];
   lesCategorie = Object.values(Category);
-  produitForm = new FormGroup({
-    id: new FormControl(0, { nonNullable: true }),
-    libelle: new FormControl('A', { nonNullable: true }),
-    prix: new FormControl(10000, { nonNullable: true }),
-    madeIn: new FormControl('Tunisie', { nonNullable: true }),
-    categorie: new FormControl(Category.Accessoires, { nonNullable: true }),
-    nouveau: new FormControl(true, { nonNullable: true }),
-  });
-  constructor(private productservice: ProductService) {}
+  // produitForm = new FormGroup({
+  //   id: new FormControl(0, { nonNullable: true }),
+  //   libelle: new FormControl('A', { nonNullable: true }),
+  //   prix: new FormControl(10000, { nonNullable: true }),
+  //   madeIn: new FormControl('Tunisie', { nonNullable: true }),
+  //   categorie: new FormControl(Category.Accessoires, { nonNullable: true }),
+  //   nouveau: new FormControl(true, { nonNullable: true }),
+  // });
+  produitForm!: FormGroup;
+  constructor(
+    private productservice: ProductService,
+    private fb: FormBuilder
+  ) {}
   onSubmitForm() {
     // console.log(this.produitForm.value);
     // console.log(this.produitForm.get('id')?.value);
@@ -36,12 +40,29 @@ export class AddproductComponent implements OnInit {
   }
   onResetForm() {
     this.produitForm.reset();
+    this.produitForm.get('madeIn')?.setValue('Autres');
+    this.produitForm.get('categorie')?.setValue(Category.Fourniture);
   }
   ngOnInit(): void {
+    this.produitForm = this.fb.nonNullable.group({
+      id: [0],
+      libelle: [''],
+      prix: [2000],
+      madeIn: ['tunisia'],
+      categorie: [Category.Accessoires],
+      nouveau: [true],
+    });
+
     this.productservice.getProducts().subscribe({
       next: (data) => {
         this.produits = data;
       },
     });
+    // code pour set value
+    this.produitForm.get('nouveau')?.setValue(false);
+    // code  de valuechanges
+    this.produitForm
+      .get('libelle')
+      ?.valueChanges.subscribe((data) => console.log(data));
   }
 }
